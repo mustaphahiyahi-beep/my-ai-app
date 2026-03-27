38824a4f1d500dadffdeb56cd7a53861fa199f41
mustaphahiyahi-beep <mustaphahiyahi@gmail.com>
2026-03-27 21:57:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 import streamlit as st
+import openai
 
-st.title("🤖 AI Chat بسيط")
+st.title("🤖 AI Chat ذكي")
+
+# ضع مفتاحك هنا
+openai.api_key = "PUT-YOUR-API-KEY-HERE"
 
 user_input = st.text_input("اكتب سؤالك:")
 
 if user_input:
-    st.write("انت قلت:", user_input)
+    response = openai.ChatCompletion.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "user", "content": user_input}
+        ]
+    )
+
+    answer = response.choices[0].message.content
+    st.write("🤖:", answer)
