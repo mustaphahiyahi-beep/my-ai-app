8c116aedf3195ecb48d3ca6537207a224bd79533
mustaphahiyahi-beep <mustaphahiyahi@gmail.com>
2026-03-27 21:52:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
-st.title("🚀 أول تطبيق AI")
-st.write("مرحبا بك!")
+st.title("🤖 AI Chat بسيط")
+
+user_input = st.text_input("اكتب سؤالك:")
+
+if user_input:
+    st.write("انت قلت:", user_input)
