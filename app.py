da0a50d393b74f1c1d49e6d70e42da765e78da97
mustaphahiyahi-beep <mustaphahiyahi@gmail.com>
2026-03-29 13:41:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from google import genai
 
-st.title("🤖 AI Assistant (Gemini New API)")
+st.title("🤖 AI Assistant")
 
 # API KEY
 client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
@@ -10,16 +10,13 @@ user_input = st.text_input("💬 اسأل:")
 
 if st.button("اسأل"):
     if user_input:
-        with st.spinner("⏳ جاري التفكير..."):
-            try:
-                response = client.models.generate_content(
-                    model="gemini-2.0-flash",
-                    contents=user_input
-                )
-
-                st.success(response.text)
-
-            except Exception as e:
-                st.error(f"خطأ: {e}")
+        try:
+            response = client.models.generate_content(
+                model="gemini-2.0-flash",
+                contents=user_input
+            )
+            st.success(response.text)
+        except Exception as e:
+            st.error(f"خطأ: {e}")
     else:
-        st.warning("اكتب سؤال أولاً")
+        st.warning("اكتب سؤال")
